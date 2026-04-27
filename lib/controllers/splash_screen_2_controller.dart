4fa2414352ebe257b53e2f65cdef8d3b628cebe0
Memon Ismail <115149149+MemonIsmail@users.noreply.github.com>
2026-04-27 17:57:33 +0500

    Delete lib directory

--- lib/controllers/splash_screen_2_controller.dart
+++
@@ -1,5 +0,0 @@
-import 'package:get/get.dart';
-
-class SplashScreen2Controller extends GetxController{
-
-}
--- lib/routing/routes.dart
+++
@@ -1,2 +0,0 @@
-const String kSplashScreenRoute = '/SPLASH_SCREEN_ROUTE';
-const String kSplashScreen2Route = '/SPLASH_SCREEN_2_ROUTE';
