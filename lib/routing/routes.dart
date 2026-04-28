38d6f4175dc874a9e00467da3dec33824728693d
Memon Ismail <115149149+MemonIsmail@users.noreply.github.com>
2026-04-28 18:11:55 +0500

    Delete lib directory

--- lib/routing/routes.dart
+++
@@ -1,4 +0,0 @@
-const String kSplashScreenRoute = '/SPLASH_SCREEN_ROUTE';
-const String kSplashScreen2Route = '/SPLASH_SCREEN_2_ROUTE';
-const String kSignupScreenRoute = '/SIGNUP_SCREEN_ROUTE';
-const String kLoginScreenRoute = '/LOGIN_SCREEN_ROUTE';
