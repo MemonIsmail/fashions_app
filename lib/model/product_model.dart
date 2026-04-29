34ded5da09d4d23016405efb3f88f7c81bdf2477
Memon Ismail <115149149+MemonIsmail@users.noreply.github.com>
2026-04-29 08:37:20 +0500

    Add files via upload

---
+++ lib/model/product_model.dart
@@ -0,0 +1,14 @@
+
+class ProductModel {
+  final String brand;
+  final String productName;
+  final String productImage;
+  final String price;
+
+  ProductModel({
+    required this.brand,
+    required this.productName,
+    required this.productImage,
+    required this.price
+  });
+}
---
+++ lib/screens/screen_colors.dart
@@ -0,0 +1,4 @@
+import 'dart:ui';
+
+const Color kWhiteColor = Color(0xFFFFFFFF);
+const Color kBlackColor = Color(0xFF000000);
