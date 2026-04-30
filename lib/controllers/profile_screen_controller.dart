dffe51b6daafddb2cd6907f666e77a445f0ca263
Memon Ismail <115149149+MemonIsmail@users.noreply.github.com>
2026-04-30 08:54:04 +0500

    Delete lib directory

--- lib/controllers/profile_screen_controller.dart
+++
@@ -1,8 +0,0 @@
-import 'package:fashions_app/routing/routes.dart';
-import 'package:get/get.dart';
-
-class ProfileScreenController extends GetxController{
-  void onLogoutPressed(){
-    Get.offAllNamed(kLoginScreenRoute);
-  }
-}
--- lib/screens/screen_colors.dart
+++
@@ -1,4 +0,0 @@
-import 'dart:ui';
-
-const Color kWhiteColor = Color(0xFFFFFFFF);
-const Color kBlackColor = Color(0xFF000000);
